616a9138fb4db6cea2152694df94e7e9c7dc472d
AsaMarjew <32294490+AsaMarjew@users.noreply.github.com>
2021-01-12 17:24:35 +0100

    Bottom bar disapere

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+document.onscroll = function() {
+    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+            document.getElementById('menu').style.bottom = '-20%';
+    }
+
+    if (window.innerHeight + window.scrollY < document.body.clientHeight) {
+            document.getElementById('menu').style.bottom = '0%';
+    }
+}
