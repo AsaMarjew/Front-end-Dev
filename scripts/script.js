fa95af5bd98355e5435ae8c5d476cb87640db86c
AsaMarjew <32294490+AsaMarjew@users.noreply.github.com>
2021-01-19 17:57:35 +0100

    update v2.0 herkansing

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
 // JavaScript Document
 
-document.onscroll = function() {
-    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
-            document.getElementById('menu').style.bottom = '-20%';
+// Deze functie zorgt ervoor dat er een menu tevoorschijnkomt wanneer er gescrollt wordt en hij verdwijnt
+// wanneer het scherm onderaan is zodat de tekst daaronder zichtbaar is.
+// Het menu verdwijnt door het te laten zakken onder het scherm (-20%).
+
+document.onscroll = function ()
+{
+    if (window.innerHeight + window.scrollY > document.body.clientHeight)
+    {
+        document.getElementById('menu').style.bottom = '-20%';
     }
 
-    if (window.innerHeight + window.scrollY < document.body.clientHeight) {
-            document.getElementById('menu').style.bottom = '0%';
+    if (window.innerHeight + window.scrollY < document.body.clientHeight)
+    {
+        document.getElementById('menu').style.bottom = '0%';
     }
-}
+};
